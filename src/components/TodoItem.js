781031fc069690f58dda6198ec193364484bb7fa
jskim1124 <jskim1124@snu.ac.kr>
2023-04-26 09:05:06 +0900

    api 업데이트

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -39,7 +39,7 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       <span
         className="ml-2 w-32 text-center"
       >
-        {todo.deadline.toString().substring(4,15)}
+        {todo.deadline}
       </span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
